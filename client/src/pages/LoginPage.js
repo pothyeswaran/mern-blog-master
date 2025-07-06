57a49ce899426b1a9300234cc01dd653d9e1dfcc
POTHYESWARAN S <pothyeswaran123@gmail.com>
2025-07-06 11:15:02 +0530

    Update LoginPage.js

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -9,7 +9,7 @@ export default function LoginPage() {
   const {setUserInfo} = useContext(UserContext);
   async function login(ev) {
     ev.preventDefault();
-    const response = await fetch('mern-blog-master-api.vercel.app/login', {
+    const response = await fetch('https://mern-blog-master-api.vercel.app/login', {
       method: 'POST',
       body: JSON.stringify({username, password}),
       headers: {'Content-Type':'application/json'},
