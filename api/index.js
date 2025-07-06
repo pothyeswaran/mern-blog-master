5315a326c4a380358e3da5cd912907c106bab0ea
POTHYESWARAN S <pothyeswaran123@gmail.com>
2025-07-06 10:40:00 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -14,7 +14,8 @@ const fs = require('fs');
 const salt = bcrypt.genSaltSync(10);
 const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 
-app.use(cors({credentials:true,origin:'mern-blog-master-sooty.vercel.app'}));
+app.use(cors({ credentials: true, origin: 'https://mern-blog-master-sooty.vercel.app' }));
+
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
