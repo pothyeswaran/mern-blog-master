90c7268f3cb82495fcc14f8e7866296a6f095d8e
POTHYESWARAN S <pothyeswaran123@gmail.com>
2025-07-06 10:21:56 +0530

    Update PostPage.js

--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -9,7 +9,7 @@ export default function PostPage() {
   const {userInfo} = useContext(UserContext);
   const {id} = useParams();
   useEffect(() => {
-    fetch(`http://localhost:4000/${id}`)
+    fetch(`mern-blog-master-api.vercel.app/${id}`)
       .then(response => {
         response.json().then(postInfo => {
           setPostInfo(postInfo);
@@ -35,7 +35,7 @@ export default function PostPage() {
         </div>
       )}
       <div className="image">
-        <img src={`http://localhost:4000/${postInfo.cover}`} alt=""/>
+        <img src={`mern-blog-master-api.vercel.app/${postInfo.cover}`} alt=""/>
       </div>
       <div className="content" dangerouslySetInnerHTML={{__html:postInfo.content}} />
     </div>
